dce51dcee08aa494c60ebb94ce701bb12205131a
Lennart Hengstmengel <github@vazaha.nl>
2021-10-12 13:33:56 +0200

    event send example

---
+++ examples/Events/DemoEvent.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Examples\Events;
+
+use Streammachine\Driver\Contracts\Event;
+
+class DemoEvent implements Event
+{
+    public $consentLevels;
+
+    public $producerSessionId;
+
+    public $url;
+
+    public $eventType;
+
+    public $referrer;
+
+    public $userAgent;
+
+    public $conversion;
+
+    public $customer;
+
+    public $abTests;
+
+    public function getStrmSchemaRef(): string
+    {
+        return 'streammachine/clickstream/1.0.0';
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'strmMeta' => [
+                'eventContractRef' => $this->getStrmSchemaRef(),
+                'nonce' => null,
+                'timestamp' => null,
+                'keyLink' => null,
+                'billingId' => null,
+                'consentLevels' => $this->consentLevels,
+            ],
+            'producerSessionId' => $this->producerSessionId,
+            'url' => $this->url,
+            'eventType' => $this->eventType,
+            'referrer' => $this->referrer,
+            'userAgent' => $this->userAgent,
+            'conversion' => $this->conversion,
+            'customer' => $this->customer,
+            'abTests' => $this->abTests,
+        ];
+    }
+}
